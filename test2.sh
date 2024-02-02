18038a957db5a3fc98d07b400c1027f6c2064616
Jaasdsa <947371421@qq.com>
2024-02-02 21:12:47 +0800

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -8,7 +8,12 @@ if [ "$ARCH" = "x86_64" ]; then
     apt-get update
     apt-get install -y google-chrome-stable
 elif [ "$ARCH" = "arm64" ]; then
-  echo "支持的CPU架构: $ARCH"
+    apt-get update
+    apt-get install -y wget gnupg ca-certificates
+    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub |  apt-key add -
+    echo "deb [arch=arm64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+    apt-get update
+    apt-get install -y google-chrome-stable
 else
   echo "不支持的CPU架构: $ARCH"
 fi
