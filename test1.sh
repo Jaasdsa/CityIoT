6c77ea75e685cf8cdbd8143974d4a05ad17d9a84
Jaasdsa <947371421@qq.com>
2024-02-02 21:06:54 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -4,7 +4,7 @@ if [ "$ARCH" = "x86_64" ]; then
     apt-get update
     apt-get install -y wget gnupg ca-certificates
     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub |  apt-key add -
-    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
     apt-get update
     apt-get install -y google-chrome-stable
 elif [ "$ARCH" = "arm64" ]; then
